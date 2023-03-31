8cf60eeff72046e4146f5838bbd5f732cd700706
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-03-31 16:37:19 -0400

    Debugged

--- linked_list.rb
+++ linked_list.rb
@@ -4,7 +4,7 @@ class Node
     @next_node = nil
   end
 
-  attr_reader :value
+  attr_reader :value, :next_node
 
   def update_next(new_next)
     self.next_node = new_next
@@ -12,7 +12,7 @@ class Node
 
   private
 
-  attr_accessor :next_node
+  attr_writer :next_node
 end
 
 class LinkedList
@@ -25,14 +25,25 @@ class LinkedList
   def append(value)
     tail = self.tail
     new_node = Node.new(value)
-    tail.update_next(new_node)
+
+    if tail == nil
+      self.root = new_node
+
+    else
+      tail.update_next(new_node)
+    end
+
     self 
   end
 
   def prepend(value)
     head = self.head
     new_node = Node.new(value)
-    new_node.update_next(head)
+
+    unless head.nil?
+      new_node.update_next(head)
+    end
+
     self.root = new_node
     self
   end
@@ -40,10 +51,12 @@ class LinkedList
   def size
     count = 0
     curr = self.head
-    until curr.next.nil?
+
+    until curr.nil?
       count += 1
-      curr = curr.next
+      curr = curr.next_node
     end
+
     count
   end
 
@@ -51,32 +64,50 @@ class LinkedList
     root
   end
 
-  def tail
+  def tail(with_prev = false)
     curr = self.head
-    until curr.next.nil?
-      curr = curr.next
+    prev = nil
+    tail = nil
+
+    until curr.nil?
+      tail = curr
+      curr = curr.next_node
+
+      unless curr.nil?
+        prev = tail 
+      end
     end
-    curr
+    return tail unless with_prev
+
+    return [tail, prev] #want prev for pop method
   end
 
   def at(index)
     i = 0
     curr = self.head
+
     while i < index && curr
       i += 1
-      curr = curr.next
+
+      curr = curr.next_node
     end
     curr
   end
 
-  def pop
-    new_tail = self.head
-    until new_tail.next.next.nil?
-      new_tail = tail.next
+  def pop 
+    tail, prev = self.tail(with_prev = true)
+
+    return nil if tail.nil? 
+
+    if prev.nil? #tail is the root 
+      self.root = nil
+
+    else
+      prev.update_next(nil)
+
     end
-    old_tail = new_tail.next
-    new_tail.update_next(nil)
-    old_tail 
+
+    tail
   end
 
   def contains?(value)
@@ -87,24 +118,30 @@ class LinkedList
     index = 0
     found = false
     curr = self.head
+
     unless curr.nil? || found
       if curr.value == value
         found = true
+
       else
         index += 1
-        curr = curr.next
+        curr = curr.next_node
       end
     end
+
     found ? index : nil
   end
 
   def to_s
     list = []
     curr = self.head
-    while curr
-      list << "( #{curr.head} )"
+
+    until curr.nil?
+      list << "( #{curr.value} )"
       list << "->"
+      curr = curr.next_node
     end
+
     list << "nil"
     list.join(" ")
   end
@@ -113,15 +150,20 @@ class LinkedList
     index = convert_index(index)
     raise RangeError if index.nil?
     
-    new_node = Node.new(value)
     if index == size
-      self.append(new_node)
-    elsif index == 1
-      self.prepend(new_node)
+      self.append(value)
+
+    elsif index == 0
+      self.prepend(value)
+
     else 
+      new_node = Node.new(value)
+
       node_before = self.at(index - 1)
-      new_node.update_next(node_before.next)
-      node_before.next = new_node
+
+      new_node.update_next(node_before.next_node)
+
+      node_before.update_next(new_node)
     end
     self
   end
@@ -131,25 +173,109 @@ class LinkedList
     raise RangeError if index.nil?
 
     removed = nil
+
     if index == 0
       removed = root
-      root = root.next
+
+      self.root = root.next_node
     else
-      node_before = at(index - 1)
-      removed = node_before.next
-      node_before.next.update_next(node_before.next.next) #if removing last, then end points to nil so node before end now points to nil
+      node_before = at(index - 1) 
+
+      removed = node_before.next_node
+
+      node_before.update_next(node_before.next_node.next_node) 
     end
+
     removed
   end
 
   def convert_index(index, remove = false)
     size = self.size
-    last_index = remove ? size - 1 : size
-    if index < 0 && index.between?(-1, -size) #insert at 0 is ok bc pushes everyone down
-      return size + index
-    elsif index >= 0 && index.between?(0, last_index) #to allow for appending to end want == to size ok
+
+    last_index = remove ? size - 1 : size 
+
+    if index < 0 && index.between?(-size, -1) 
+      return remove ? size + index : size + index + 1
+
+    elsif index >= 0 && index.between?(0, last_index) 
       return index
     end
+
     nil
   end
+end
+
+my_list = LinkedList.new
+puts my_list # => nil
+
+my_list.append(1)
+puts my_list # => ( 1 ) -> nil
+
+my_list.prepend(2)
+puts my_list # => ( 2 ) -> ( 1 ) -> nil
+
+puts my_list.head.value # => 2
+
+puts my_list.tail.value # => 1
+
+puts my_list.at(1).value # => 1
+
+puts my_list.find(2) # => 0
+
+pp my_list.find(3) # => nil
+
+puts my_list.contains?(2) # => true
+puts my_list.contains?(3) # => false
+
+my_list.insert_at(3, 0) 
+puts my_list # => ( 3 ) -> ( 2 ) -> ( 1 ) -> nil
+
+my_list.insert_at(5, 1)
+puts my_list # => ( 3 ) -> ( 5 ) -> ( 2 ) -> ( 1 ) -> nil
+
+removed = my_list.pop
+puts removed.value # => 1
+puts my_list # => ( 3 ) -> ( 5 ) -> ( 2 ) -> nil
+
+
+my_list.insert_at(6, -1) 
+puts my_list # => ( 3 ) -> ( 5 ) -> ( 2 ) -> ( 6 ) -> nil
+
+my_list.insert_at(7, -2)
+puts my_list # => ( 3 ) -> ( 5 ) -> ( 2 ) -> ( 7 ) -> ( 6 ) -> nil
+
+my_list.remove_at(0)
+puts my_list # => ( 5 ) -> ( 2 ) -> ( 7 ) -> ( 6 ) -> nil
+
+my_list.remove_at(-1)
+puts my_list  # => ( 5 ) -> ( 2 ) -> ( 7 ) -> nil
+
+begin 
+  my_list.insert_at(100, 10)
+rescue RangeError
+  puts "Received expected error"
+end 
+
+begin 
+  my_list.insert_at(100, -10)
+rescue RangeError
+  puts "Received expected error"
+end 
+
+begin 
+  my_list.remove_at(10)
+rescue RangeError
+  puts "Received expected error"
+end
+
+begin 
+  my_list.remove_at(-10)
+rescue RangeError
+  puts "Received expected error"
+end
+
+begin 
+  my_list.remove_at(3)
+rescue RangeError
+  puts"Received expected error"
 end
